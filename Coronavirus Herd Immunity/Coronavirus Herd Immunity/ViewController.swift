3b559e1cb86256d59e289ea8318f1696e6d00aae
Antonio Romano <tonino.roma@gmail.com>
2020-02-23 17:47:53 +0100

    Startup xcode project

---
+++ Coronavirus Herd Immunity/Coronavirus Herd Immunity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coronavirus Herd Immunity
+//
+//  Created by Antonio Romano on 23/02/2020.
+//  Copyright © 2020 Coronavirus-Herd-Immunity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
