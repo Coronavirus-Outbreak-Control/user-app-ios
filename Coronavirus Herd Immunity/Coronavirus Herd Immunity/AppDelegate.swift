903ad01e2dbe32c69e9d41bdd189af92eb754810
Antonio Romano <tonino.roma@gmail.com>
2020-03-07 15:57:19 +0100

    Background fetch

--- Coronavirus Herd Immunity/Coronavirus Herd Immunity/AppDelegate.swift
+++ Coronavirus Herd Immunity/Coronavirus Herd Immunity/AppDelegate.swift
@@ -34,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //            }
             StorageManager.shared.setIdentifierDevice(Utils.randomInt())
         }
+        UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
     }
     
 //    func application(_ application: UIApplication,
@@ -52,6 +53,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //            backgroundCompletionHandler()
 //        }
 //    }
+    
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("background fetchs")
+        CoreManager.pushInteractionsInBackground()
+        completionHandler(.newData)
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
